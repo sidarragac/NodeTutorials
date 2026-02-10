3cb90e3ba89cf37b6348c9e35ad50bc70354e9d0
Santiago Idárraga Ceballos <san200526@gmail.com>
2026-02-10 17:29:47 -0500

    Completa tutorial con tarea

---
+++ frontend/src/views/ContactView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="mt-4 mb-4">
+    <div class="max-w-7xl mx-auto">
+      <h2 class="text-3xl font-bold text-gray-800 text-center">Contact Page</h2>
+      <h4 class="text-3xl font-bold text-gray-800 text-center">
+        Desarrollado por: Santiago Idárraga Ceballos
+      </h4>
+    </div>
+  </section>
+</template>
