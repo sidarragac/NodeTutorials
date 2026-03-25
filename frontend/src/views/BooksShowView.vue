def2a840b13e0d89c1b792432f7ff5c6600372af
Santiago Idárraga Ceballos <san200526@gmail.com>
2026-03-24 21:49:32 -0500

    Cambia componente y vista

--- frontend/src/views/BooksShowView.vue
+++ frontend/src/views/BooksShowView.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
+import { onMounted, ref } from 'vue';
 
+
+import type { BookInterface } from '@/interfaces/BookInterface.js';
 import BookReviews from '@/components/BookReviews.vue';
 import { BookService } from '@/services/BookService.js';
 
-const route = useRoute();
-const bookId = Number(route.params.id);
-const book = BookService.getBookById(bookId);
+const book = ref<BookInterface | null>(null);
 
 // functions
 function formatToCOP(price: number): string {
@@ -19,6 +20,13 @@ function formatToCOP(price: number): string {
 
   return formatter.format(price).replace(/^\s*\$\s?/, '');
 }
+
+onMounted(async () => {
+  const route = useRoute();
+  const bookId = Number(route.params.id);
+  book.value = await BookService.getBookById(bookId);
+});
+
 </script>
 
 <template>
