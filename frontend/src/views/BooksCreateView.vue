f1ae3a038f63ad6e8ebbca77cdf9d01142042b94
Santiago Idárraga Ceballos <san200526@gmail.com>
2026-03-24 21:27:22 -0500

    Modifica vistas para usar nuevas funciones

--- frontend/src/views/BooksCreateView.vue
+++ frontend/src/views/BooksCreateView.vue
@@ -9,21 +9,25 @@ const price = ref(0);
 const stock = ref(0);
 const successMessage = ref('');
 
-function submitForm() {
+async function submitForm() {
   const newBook: CreateBookDTO = {
     title: title.value,
     category: category.value,
     price: price.value,
     stock: stock.value,
   };
-
-  BookService.createBook(newBook);
-  successMessage.value = 'Book created successfully!';
-  title.value = '';
-  category.value = '';
-  price.value = 0;
-  stock.value = 0;
+  try {
+    await BookService.createBook(newBook);
+    successMessage.value = 'Book created successfully!';
+    title.value = '';
+    category.value = '';
+    price.value = 0;
+    stock.value = 0;
+  } catch (error) {
+    console.error(error);
+  }
 }
+
 </script>
 
 <template>
