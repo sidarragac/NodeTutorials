0fcd889854314bd65d60052795705c91b4462715
Santiago Idárraga Ceballos <san200526@gmail.com>
2026-03-24 21:32:35 -0500

    Soluciona problemas con imports

--- backend/src/books/books.module.ts
+++ backend/src/books/books.module.ts
@@ -4,8 +4,8 @@ import { BooksService } from './books.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
 import { Review } from './entities/review.entity';
-import { ReviewsController } from './review.controller';
-import { ReviewsService } from './review.service';
+import { ReviewsController } from './reviews.controller';
+import { ReviewsService } from './reviews.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book, Review])],
