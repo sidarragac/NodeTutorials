e2018d4338f47977f863cb32c21a915bb4fc7c96
Santiago Idárraga Ceballos <san200526@gmail.com>
2026-03-24 21:30:33 -0500

    Agrega servicios y controladores de Reviews

--- backend/src/books/books.module.ts
+++ backend/src/books/books.module.ts
@@ -3,10 +3,13 @@ import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
+import { Review } from './entities/review.entity';
+import { ReviewsController } from './review.controller';
+import { ReviewsService } from './review.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
-  controllers: [BooksController],
-  providers: [BooksService],
+  imports: [TypeOrmModule.forFeature([Book, Review])],
+  controllers: [BooksController, ReviewsController],
+  providers: [BooksService, ReviewsService],
 })
 export class BooksModule {}
--- backend/src/books/entities/book.entity.ts
+++ backend/src/books/entities/book.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Review } from './review.entity';
 
 @Entity()
 export class Book {
@@ -16,4 +17,7 @@ export class Book {
 
   @Column()
   stock: number;
+
+  @OneToMany(() => Review, (review) => review.book)
+  reviews: Review[];
 }
